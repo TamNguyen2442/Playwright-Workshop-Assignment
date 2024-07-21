5a885a5791215e6704169d34043feb128575793c
TamNguyen2442 <124868115+TamNguyen2442@users.noreply.github.com>
2024-07-21 20:24:51 +0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,19 +18,25 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  //workers: process.env.CI ? 1 : undefined,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    //'html',
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    video:'retain-on-failure',
-    screenshot:'only-on-failure',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    viewport: { width: 1920, height: 1080 },
     trace: 'on-first-retry',
     ignoreHTTPSErrors: true
   },
